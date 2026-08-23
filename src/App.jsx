5af0a9dc91070a4a1927749ff7f08078267fd928
Belen Baez <bnoeliabelen@gmail.com>
2026-08-22 21:51:29 -0300

    Eliminar lógica de estado y agregar componente DashboardRecepcion

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,12 @@
-import { useState } from 'react';
-import { Button, Container, Badge } from 'react-bootstrap';
+import DashboardRecepcion from "./pages/DashboardRecepcion"
 
 function App() {
-  const [estadoPaciente, setEstadoPaciente] = useState("Pendiente");
-
-  const atender = () => {
-    setEstadoPaciente("Atendido");
-    console.log("Variable en memoria:", estadoPaciente);
-  }
 
   return (
     <>
-      <Container className="mt-5">
-        <h2>Estado del turno: 
-          <Badge bg={estadoPaciente === "Atendido" ? "success" : "warning"} className="ms-2">
-            {estadoPaciente}
-          </Badge>
-        </h2>
-        <Button onClick={atender} disabled={estadoPaciente === "Atendido"}>
-          Llamar paciente
-        </Button>
-      </Container>
+      <DashboardRecepcion></DashboardRecepcion>
     </>
   )
 }
 
-export default App;
+export default App
---
+++ src/pages/dashboardRecepcion.jsx
@@ -0,0 +1,59 @@
+import { useState } from "react";
+import { Container, Badge, Row, Col, Card, Button } from "react-bootstrap";
+
+const turnosDelDia = [
+    { id: 1, pacientes: "Tomas Gallardo", especialidad: "Cardiologia", estado: "pendiente"},
+    { id: 2, pacientes: "Marina Lopez", especialidad: "Clinica Medica", estado: "pendiente"},
+    { id: 3, pacientes: "Susana", especialidad: "Oftalmologia", estado: "pendiente"},
+    { id: 4, pacientes: "Roque Gimenez", especialidad: "Pediatria", estado: "pendiente"},
+    { id: 5, pacientes: "Ernesto Roura", especialidad: "Cardiologia", estado: "pendiente"},
+    { id: 6, pacientes: "Juan Pared", especialidad: "Cardiologia", estado: "pendiente"},
+    { id: 7, pacientes: "Marisol Nuñez", especialidad: "Cardiologia", estado: "pendiente"},
+    { id: 8, pacientes: "Tomas Gallardo", especialidad: "Cardiologia", estado: "pendiente"},
+    { id: 9, pacientes: "Marina Lopez", especialidad: "Clinica Medica", estado: "pendiente"},
+    { id: 10, pacientes: "Susana", especialidad: "Oftalmologia", estado: "pendiente"},
+    { id: 11, pacientes: "Roque Gimenez", especialidad: "Pediatria", estado: "pendiente"},
+    { id: 12, pacientes: "Ernesto Roura", especialidad: "Cardiologia", estado: "pendiente"},
+    { id: 13, pacientes: "Juan Pared", especialidad: "Cardiologia", estado: "pendiente"},
+    { id: 14, pacientes: "Marisol Nuñez", especialidad: "Cardiologia", estado: "pendiente"}
+];
+
+const DashboardRecepcion = () => {
+    const [turnos, setTurnos] = useState(turnosDelDia);
+
+    const marcarComoAtendido = (idTurno) => {
+        const turnosActualizados = turnos.map(turno => {
+            if (turno.id === idTurno) return { ...turno, estado: "Atendido"};
+            return turno;
+        });
+        setTurnos(turnosActualizados);
+    };
+
+    return (
+        <Container className="mt-4">
+            <h2 className="mb-4">Turnos del Día</h2>
+            <Row>
+                {turnos.map((turno) => (
+                  <Col md={4} key={turno.id} className="mb-3">
+                    <Card>
+                        <Card.Body>  
+                          <Card.Title>{turno.pacientes}</Card.Title>
+                          <h5 className="mt-3">
+                              {turno.estado === 'Atendido' 
+                                  ? <Badge bg="success">Atendido</Badge>
+                                  : <Badge bg="warning" text="dark">En Espera</Badge>
+                              }
+                          </h5>
+                          <Button onClick={() => marcarComoAtendido(turno.id)} disabled={turno.estado === 'Atendido'}>
+                            Llamar
+                          </Button>
+                        </Card.Body>
+                    </Card>
+                  </Col>
+                ))}
+            </Row>
+        </Container>
+    );
+};
+
+export default DashboardRecepcion;
