f851adf0e7a063ead850efc7c3341c401134d441
Belen Baez <bnoeliabelen@gmail.com>
2026-08-29 19:35:18 -0300

    agregar validaciones en formulario.

--- src/components/pacientes/FormularioPaciente.jsx
+++ src/components/pacientes/FormularioPaciente.jsx
@@ -1,98 +1,216 @@
-import { useState } from "react";
-import styles from "./FormularioPaciente.module.scss";
-import JsonDebugger from "../utils/jsondebugger";
+import { useState } from 'react';
+import styles from './FormularioPaciente.module.scss';
+import JsonDebugger from '../utils/JsonDebugger';
+import { Button } from 'react-bootstrap';
+import { validarDatos } from '../utils/validaciones';
+
+const reglasPaciente = {
+    nombre: (valor) => valor.trim() === "" ? "El nombre es obligatorio." : null,
+    dni: (valor) => valor.length < 8 ? "El DNI debe tener 8 numeros minimo" : null,
+    email: (valor) => !valor.includes("@") ? "Debe ser un correo valido. " : null
+};
+
 
 const FormularioPaciente = () => {
     const [paciente, setPaciente] = useState({
         nombre: "",
-        apellido: "",
         dni: "",
         email: "",
-        telefono: "",
-        fechaNacimiento: "",
-        direccion: ""
+        direccion: {
+            calle: "",
+            numero: "",
+            piso: "",
+            departamento: "",
+            barrio: ""
+        },
+        telefono: {
+            tipo: "CELULAR", 
+            codigoArea: "",
+            numero: ""
+        },
+        obraSocial: {
+            nombre: "",
+            numeroAfiliado: ""
+        },
+        historialMedico: {
+            fecha: "",
+            diagnostico: "",
+            tratamiento: "",
+            medico: ""
+        }
     });
 
-    const handleChange = (event) => {
-        const { name, value } = event.target;
+    const [errores, setErrores] = useState({});
 
-        setPaciente({
-            ...paciente,
-            [name]: value
-        });
+    const handleChange = (evento) => {
+        const { name, value } = evento.target;
+
+        if (name.includes('.')) {
+            const [seccion, propiedad] = name.split('.'); 
+            
+            setPaciente({
+                ...paciente,
+                [seccion]: {
+                    ...paciente[seccion], 
+                    [propiedad]: value    
+                }
+            });
+        } else {
+            setPaciente({
+                ...paciente,
+                [name]: value
+            });
+        }
+    };
+
+    const handleSubmit = async (evento) => {
+
+        evento.preventDefault();
+
+        const nuevosErrores = validarDatos(paciente, reglasPaciente);
+
+        setErrores(nuevosErrores);
+
+        if (Object.keys(nuevosErrores).length > 0) {
+            console.log("Validacion fallida");
+            return;
+        }
+
+        try {
+
+            const respuesta = await fetch("http://localhost:3000/api/v1/pacientes", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify(paciente)
+            });
+
+            const data = await respuesta.json();
+
+            if (respuesta.ok) {
+                alert("Paciente gaardado en base de datos");
+            } else {
+                alert("error del servidor: " + data.message + "errores: " +data.data);
+            }
+
+        } catch (error) {
+            console.error("Error de conexion", error);
+            alert("el servidor esta apagado o no responde");
+        }
+
+        console.log(paciente);
     };
 
     return (
         <div className={styles.contenedorFormulario}>
             <h3>Ingreso de Nuevo Paciente</h3>
+            <form onSubmit={handleSubmit}>
+                {/* --- DATOS PERSONALES --- */}
+                <fieldset>
+                    <legend>Datos Personales</legend>
+                    <input 
+                        type="text" name="nombre" value={paciente.nombre} 
+                        className={styles.campoInput} placeholder="Nombre completo" onChange={handleChange} 
+                    />
+                    {errores.nombre && <span className={styles.textoError}>{errores.nombre}</span>}
+                    <input 
+                        type="text" name="dni" value={paciente.dni} 
+                        className={styles.campoInput} placeholder="DNI" onChange={handleChange} 
+                    />
+                    {errores.dni && <span className={styles.textoError}>{errores.dni}</span>}
+                    <input 
+                        type="email" name="email" value={paciente.email} 
+                        className={styles.campoInput} placeholder="Email" onChange={handleChange} 
+                    />
+                    {errores.email && <span className={styles.textoError}>{errores.email}</span>}
+                </fieldset>
 
-            <form>
-                <input
-                    type="text"
-                    name="nombre"
-                    value={paciente.nombre}
-                    className={styles.campoInput}
-                    placeholder="Nombre"
-                    onChange={handleChange}
-                />
+                {/* --- DIRECCIÓN --- */}
+                <fieldset>
+                    <legend>Dirección</legend>
+                    <input 
+                        type="text" name="direccion.calle" value={paciente.direccion.calle} 
+                        className={styles.campoInput} placeholder="Calle" onChange={handleChange} 
+                    />
+                    <input 
+                        type="text" name="direccion.numero" value={paciente.direccion.numero} 
+                        className={styles.campoInput} placeholder="Número" onChange={handleChange} 
+                    />
+                    <input 
+                        type="text" name="direccion.piso" value={paciente.direccion.piso} 
+                        className={styles.campoInput} placeholder="Piso" onChange={handleChange} 
+                    />
+                    <input 
+                        type="text" name="direccion.departamento" value={paciente.direccion.departamento} 
+                        className={styles.campoInput} placeholder="Departamento" onChange={handleChange} 
+                    />
+                    <input 
+                        type="text" name="direccion.barrio" value={paciente.direccion.barrio} 
+                        className={styles.campoInput} placeholder="Barrio" onChange={handleChange} 
+                    />
+                </fieldset>
 
-                <input
-                    type="text"
-                    name="apellido"
-                    value={paciente.apellido}
-                    className={styles.campoInput}
-                    placeholder="Apellido"
-                    onChange={handleChange}
-                />
+                {/* --- TELÉFONO --- */}
+                <fieldset>
+                    <legend>Teléfono</legend>
+                    <select 
+                        name="telefono.tipo" value={paciente.telefono.tipo} 
+                        className={styles.campoInput} onChange={handleChange}
+                    >
+                        <option value="CELULAR">Celular</option>
+                        <option value="FIJO">Fijo</option>
+                    </select>
+                    <input 
+                        type="text" name="telefono.codigoArea" value={paciente.telefono.codigoArea} 
+                        className={styles.campoInput} placeholder="Código de Área" onChange={handleChange} 
+                    />
+                    <input 
+                        type="text" name="telefono.numero" value={paciente.telefono.numero} 
+                        className={styles.campoInput} placeholder="Número de Teléfono" onChange={handleChange} 
+                    />
+                </fieldset>
 
-                <input
-                    type="text"
-                    name="dni"
-                    value={paciente.dni}
-                    className={styles.campoInput}
-                    placeholder="DNI"
-                    onChange={handleChange}
-                />
+                {/* --- OBRA SOCIAL --- */}
+                <fieldset>
+                    <legend>Obra Social</legend>
+                    <input 
+                        type="text" name="obraSocial.nombre" value={paciente.obraSocial.nombre} 
+                        className={styles.campoInput} placeholder="Nombre Obra Social" onChange={handleChange} 
+                    />
+                    <input 
+                        type="text" name="obraSocial.numeroAfiliado" value={paciente.obraSocial.numeroAfiliado} 
+                        className={styles.campoInput} placeholder="Nº de Afiliado" onChange={handleChange} 
+                    />
+                </fieldset>
 
-                <input
-                    type="email"
-                    name="email"
-                    value={paciente.email}
-                    className={styles.campoInput}
-                    placeholder="Email"
-                    onChange={handleChange}
-                />
+                {/* --- HISTORIAL MÉDICO --- */}
+                <fieldset>
+                    <legend>Historial Médico</legend>
+                    <input 
+                        type="datetime-local" name="historialMedico.fecha" value={paciente.historialMedico.fecha} 
+                        className={styles.campoInput} onChange={handleChange} 
+                    />
+                    <input 
+                        type="text" name="historialMedico.diagnostico" value={paciente.historialMedico.diagnostico} 
+                        className={styles.campoInput} placeholder="Diagnóstico" onChange={handleChange} 
+                    />
+                    <input 
+                        type="text" name="historialMedico.tratamiento" value={paciente.historialMedico.tratamiento} 
+                        className={styles.campoInput} placeholder="Tratamiento" onChange={handleChange} 
+                    />
+                    <input 
+                        type="text" name="historialMedico.medico" value={paciente.historialMedico.medico} 
+                        className={styles.campoInput} placeholder="Médico a cargo" onChange={handleChange} 
+                    />
+                </fieldset>
 
-                <input
-                    type="text"
-                    name="telefono"
-                    value={paciente.telefono}
-                    className={styles.campoInput}
-                    placeholder="Teléfono"
-                    onChange={handleChange}
-                />
-
-                <input
-                    type="date"
-                    name="fechaNacimiento"
-                    value={paciente.fechaNacimiento}
-                    className={styles.campoInput}
-                    onChange={handleChange}
-                />
-
-                <input
-                    type="text"
-                    name="direccion"
-                    value={paciente.direccion}
-                    className={styles.campoInput}
-                    placeholder="Dirección"
-                    onChange={handleChange}
-                />
+                <Button type="submit">Guardar </Button>
             </form>
 
             <JsonDebugger
                 data={paciente}
-                titulo="Estado actual del JSON del paciente"
+                titulo="ESTADO DEL JSON"
             />
         </div>
     );
